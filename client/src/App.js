de1986d89d35e048c079e71a173d0bd8ad34d9f3
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-21 19:53:54 -0600

    organized into client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import Header from './components/Header'
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div>
+      {/* page state */}
+      </div>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return <nav>
-
+        this is my Header
     </nav>
 }
 

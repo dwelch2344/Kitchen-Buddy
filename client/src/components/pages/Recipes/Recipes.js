6f6ee23b605c602514a49cef5448ba6d4d77bb86
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-23 12:56:41 -0600

    added api for recipes to be able to pull from the edamam api

---
+++ client/src/components/pages/Recipes/Recipes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const recipes = () => {
+    const appID = 'e2f68dc2';
+    const appKey = '7b48db1896534b041d46a13fee1d2b99';
+
+    const example = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=e2f68dc2&app_key=7b48db1896534b041d46a13fee1d2b99`
+
+    return (
+        <div className='app'>
+            <h1>Hello!</h1>!
+        </div>
+    );
+};
+
+export default recipes;

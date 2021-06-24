e1df616fc7119334f684929af7598828b3b5ae90
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-23 22:42:25 -0600

    working on styling / half done

--- client/src/components/pages/ConvertPage/ConvertPage.js
+++ client/src/components/pages/ConvertPage/ConvertPage.js
@@ -6,24 +6,41 @@ const Converter = () => {
 
     return (
         <div className="converter-container">
-            <div className="from-and-to">
+            
+            <div className="converter-app-container">
 
-                <select className="convert-from" placeholder="from"></select>
+                <div className="converter-app">
 
-                <select className="convert-to" placeholder="to"></select>
+                    <div className="from-and-to">
 
-            </div>
+                        <select className="convert-from" placeholder="from">
+                            <option value="" disabled selected hidden>convert from</option>
+                            <option value="cat">cat</option>
+                            <option value="cat">cat</option>
+                            <option value="cat">cat</option>
+                        </select>
 
-            <div className="init-value-and-submit-btn">
+                        <select className="convert-to" placeholder="to">
+                            <option value="" disabled selected hidden >convert to</option>
+                            <option value="cat">cat</option>
+                            <option value="cat">cat</option>
+                            <option value="cat">cat</option>
+                        </select>
 
-                <input className="initial-value" placeholder="initial value"></input>
+                    </div>
 
-                <button className="convert-btn">convert!</button>
+                    <div className="init-value-and-submit-btn">
 
-            </div>
+                        <input className="initial-value" placeholder="initial value"></input>
 
-            <div className="returned-value-container" placeholder="result">
-                {/* <div className="returned-value">{converter.result}</div> */}
+                        <button className="convert-btn">convert!</button>
+
+                    </div>
+
+                    <div className="returned-value-container" placeholder="result">
+                        {/* <div className="returned-value">{converter.result}</div> */}
+                    </div>
+                </div>
             </div>
         </div>
     );

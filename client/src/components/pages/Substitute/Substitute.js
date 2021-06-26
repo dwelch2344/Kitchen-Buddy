513fd351989fb2692019a27f5954e3555e5c7ec4
Valdeth <alstontravis6@gmail.com>
2021-06-25 20:48:57 -0600

    HTML for subsitiutions

---
+++ client/src/components/pages/Substitute/Substitute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Auth from '../utils/auth';
+
+const Subsitute = () => {
+
+    const loggedIn = Auth.loggedin();
+
+    return (
+        <main>
+            <div>
+
+            </div>
+        </main> 
+    );
+};
+
+export default Subsitute;

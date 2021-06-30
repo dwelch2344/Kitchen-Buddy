1bea85abcd5df778a95f37961ee9601fe197bf14
Javid De Los Santos <javiddelossantos5@gmail.com>
2021-06-29 19:27:40 -0600

    changes made

--- client/src/components/Recipes/recipe.js
+++ client/src/components/Recipes/recipe.js
@@ -6,10 +6,12 @@ const Recipe = ({ title, calories, url, image, ingredients, labels }) => {
         <div className='recipe'>
             <h1 className='title'>{title}</h1>
             <ol>
-                <h2 className='label'>Health Label</h2>
-                {labels.map(label => (
+                {/* <h2 className='label'>Health Label</h2>
+
+                if wanting a health label */}
+                {/* {labels.map(label => (
                     <li>{[label]}</li>
-                ))}
+                ))} */}
             </ol>
             <ol>
                 {ingredients.map(ingredient => (
